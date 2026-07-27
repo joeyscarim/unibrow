4ae31d869ddec539e10a062c1a82574561b94f07
Joey Scarim <joey@gomoonshot.com>
2026-07-27 16:39:41 -0600

    Initial Commit

---
+++ unibrow/unibrowApp.swift
@@ -0,0 +1,17 @@
+//
+//  unibrowApp.swift
+//  unibrow
+//
+//  Created by Joey Scarim on 7/27/26.
+//
+
+import SwiftUI
+
+@main
+struct unibrowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
